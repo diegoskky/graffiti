de76ba8a6fc0ea90e40c9e0e91ff0b74a6e5ade4
Miguel <dreamer.5122@gmail.com>
2017-04-20 04:54:06 -0400

    Implementación del automata, es bastante rudimentario pero funciona, falta hacerle pruebas en conjunto

---
+++ src/sample/Afnd.java
@@ -0,0 +1,69 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Afnd {
+    private String alfabeto;
+    private Nodo estadoInicial;
+    private ArrayList<Nodo> estados;
+
+    public Afnd(String alfabeto){
+        this.alfabeto = alfabeto;
+        this.estados = new ArrayList<>();
+        this.estadoInicial = new Nodo();
+    }
+
+    public Nodo getEstadoInicial() {
+        return estadoInicial;
+    }
+
+    public void setEstadoInicial(Nodo estadoInicial) {
+        this.estadoInicial = estadoInicial;
+    }
+
+    public void addEstado(Nodo nuevo){
+        this.estados.add(nuevo);
+    }
+
+    public boolean comprobarAlfabeto(char letra){
+        if(alfabeto.contains(Character.toString(letra))){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean comprobarPalabra(String palabra){
+        char letra;
+        String aux = palabra;
+        Nodo estadoActual = new Nodo();
+
+        letra = palabra.charAt(0);
+        aux = aux.substring(1);
+
+        if(comprobarAlfabeto(letra)){
+            if(estadoInicial.comprobarTransicion(letra)){
+                estadoInicial.getIndiceTransicion(letra);
+                estadoActual = estadoInicial.getEstadoLlegada(letra);
+                System.out.println("esta aqui: "+estadoActual.getEstado());
+
+                while(!aux.trim().isEmpty()){
+                    letra = aux.charAt(0);
+                    aux = aux.substring(1);
+
+                    if(estadoActual.comprobarTransicion(letra)){
+                        estadoActual.getIndiceTransicion(letra);
+                        estadoActual = estadoActual.getEstadoLlegada(letra);
+                        System.out.println("esta aqui: "+estadoActual.getEstado());
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if(estadoActual.getEsFinal()){
+            return true;
+        }
+        return false;
+    }
+}
--- src/sample/Nodo.java
+++ src/sample/Nodo.java
@@ -1,7 +1,62 @@
 package sample;
 
-/**
- * Created by Freyja on 18-04-2017.
- */
+import java.util.ArrayList;
+
 public class Nodo {
+    private char estado;
+    private boolean esFinal;
+    private ArrayList<Transicion> transiciones;
+
+    public Nodo(){
+        this.estado = ' ';
+        this.esFinal = false;
+        this.transiciones = new ArrayList<>();
+    }
+
+    public char getEstado() {
+        return estado;
+    }
+
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+
+    public boolean getEsFinal() {
+        return esFinal;
+    }
+
+    public void setEsFinal(boolean esFinal) {
+        this.esFinal = esFinal;
+    }
+
+    public void addTransicion(Transicion nuevo){
+        this.transiciones.add(nuevo);
+    }
+
+    public boolean comprobarTransicion(char aux){
+        for(Transicion i : transiciones){
+            if(aux == i.getTransicion()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getSize(){
+        return transiciones.size();
+    }
+
+    public int getIndiceTransicion(char aux){
+        for(int i = 0; i < transiciones.size(); i++){
+            if(aux == transiciones.get(i).getTransicion()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Nodo getEstadoLlegada(char aux){
+        return transiciones.get(getIndiceTransicion(aux)).getEstadoLlegada();
+    }
+
 }
