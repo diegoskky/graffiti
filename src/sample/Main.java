9d8b72af1cb94d87adaab45caa8b35f3f7b3ccb5
Dark7Zero <carias14@alumnos.utalca.cl>
2017-04-24 23:55:22 -0400

    Recursos y GridPane para trabajar, opcion con el menu lateral agregado, con pesimos colores!!!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,35 +1,60 @@
 package sample;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
+import javafx.util.Duration;
 
 public class Main extends Application implements EventHandler<ActionEvent> {
 
     private int width,height;
-    private Button leftBtn,rightBtn,optionBtn,saveBtn;
+    private Button leftBtn,rightBtn,saveBtn;
+    private Button optionMenu;
+    private Button addNodeBtn;
+    private Button exitBtn;
+    private boolean addNodeClicked= false;
+    private StackPane root;
+    private BorderPane todo;
+    private AnchorPane editorRoot;
+    private HBox fileRoot;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.width=800;
         this.height=600;
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        BorderPane root= new BorderPane();
+        this.root = new StackPane();
+        this.todo= new BorderPane();
+        this.editorRoot = new AnchorPane();
+        this.optionMenu= new Button();
+        this.optionMenu.setPrefSize(30,30);
+        this.optionMenu.setStyle("-fx-background-image: url('/resources/Option.png');" +
+                "-fx-backgound-size: 100% 100%;" +
+                "-fx-background-repeat: no-repeat;" +
+                "-fx-background-size: contain;" +
+                "-fx-rotate: 180");
+        this.optionMenu.setOnAction(this);
+        root.getChildren().addAll(editorRoot,todo);
 
         //panel top
         GridPane top= new GridPane();
@@ -37,18 +62,12 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         top.setVgap(10);
 
         //panel top 0
-        this.optionBtn= new Button();
-        this.optionBtn.setPrefSize(30,30);
-        this.optionBtn.setStyle("-fx-background-image: url('/resources/Option.png');" +
-                "-fx-backgound-size: 100% 100%;" +
-                "-fx-background-repeat: no-repeat;" +
-                "-fx-background-size: contain;");
-        this.optionBtn.setOnAction(this);
+
         HBox subTopPane0= new HBox();
-        subTopPane0.setAlignment(Pos.CENTER_LEFT);
+        subTopPane0.setAlignment(Pos.CENTER);
         subTopPane0.setPadding(new Insets(5,10,5,10));
         subTopPane0.setSpacing(5);
-        subTopPane0.getChildren().addAll(this.optionBtn);
+        subTopPane0.getChildren().addAll(this.optionMenu);
 
         //panel top 1
         this.leftBtn= new Button();
@@ -91,16 +110,21 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         top.add(subTopPane2,2,0);
 
         //Panel center
+        ScrollPane center= new ScrollPane();
+        center.setPannable(true);
+        center.setPrefSize(500,600);
+        center.setContent(new Lienzo());
+
         //panel right
         Pane right=new Pane();
         right.setPrefSize(200,500);
         right.getChildren().add(new Label(" crear entradas  "));
 
         //Configuracion del borderPane
-        root.setTop(top);
-        root.setCenter(new Lienzo());
-        root.setRight(right);
-        root.setPadding(new Insets(10,10,10,10));
+        todo.setTop(top);
+        todo.setCenter(center);
+        todo.setRight(right);
+        todo.setPadding(new Insets(10,10,10,10));
 
         //Configuracion de la pantalla principal
         primaryStage.setTitle("Graph Drawer");
@@ -108,6 +132,77 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         primaryStage.setScene(new Scene(root, this.width, this.height));
         primaryStage.getIcons().add(new Image("/resources/icon.png"));
         primaryStage.show();
+
+        //Menu lateral izquierdo despegable
+
+        this.fileRoot = new HBox();
+        VBox menu = new VBox();
+        menu.setStyle("-fx-background-color: lawngreen;");
+        menu.setFillWidth(true);
+        Button backBtn = new Button("Options");
+        backBtn.setPrefWidth(150);
+        backBtn.setStyle("-fx-background-color: green;\n" +
+                "    -fx-text-fill: white;\n" +
+                "    -fx-border: none; ");
+        backBtn.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                FadeTransition hideFileRootTransition = new FadeTransition(Duration.millis(500), fileRoot);
+                hideFileRootTransition.setFromValue(1.0);
+                hideFileRootTransition.setToValue(0.0);
+
+                FadeTransition showEditorRootTransition = new FadeTransition(Duration.millis(500), editorRoot);
+                showEditorRootTransition.setFromValue(0.0);
+                showEditorRootTransition.setToValue(1.0);
+
+                showEditorRootTransition.play();
+                hideFileRootTransition.play();
+                root.getChildren().remove(fileRoot);
+            }
+        });
+        // agregar aqui botones del panel lateral
+        ImageView addNodeImage= new ImageView(new Image("/resources/add-circle.png"));
+        addNodeImage.setPreserveRatio(true);
+        addNodeImage.setFitHeight(40);
+        addNodeImage.setFitWidth(40);
+        this.addNodeBtn= new Button("add node",addNodeImage);
+        this.addNodeBtn.setOnAction(this);
+        this.addNodeBtn.setPrefWidth(150);
+        this.addNodeBtn.setStyle("-fx-text-fill: white;" +
+                "-fx-border: none;" +
+                "-fx-background-color: lawngreen");
+
+        ImageView exitImage= new ImageView(new Image("/resources/exit.png"));
+        exitImage.setPreserveRatio(true);
+        exitImage.setFitHeight(40);
+        exitImage.setFitWidth(40);
+        this.exitBtn= new Button("exit",exitImage);
+        this.exitBtn.setOnAction(this);
+        this.exitBtn.setPrefWidth(150);
+        this.exitBtn.setStyle("-fx-text-fill: white;" +
+                "-fx-border: none;" +
+                "-fx-background-color: lawngreen;");
+
+
+        menu.getChildren().addAll(backBtn,this.addNodeBtn, this.exitBtn);
+        VBox.setVgrow(this.addNodeBtn, Priority.ALWAYS);
+        fileRoot.getChildren().add(menu);
+
+        this.optionMenu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                root.getChildren().add(fileRoot);
+                FadeTransition hideEditorRootTransition = new FadeTransition(Duration.millis(500), editorRoot);
+                hideEditorRootTransition.setFromValue(1.0);
+                hideEditorRootTransition.setToValue(0.0);
+
+                FadeTransition showFileRootTransition = new FadeTransition(Duration.millis(500), fileRoot);
+                showFileRootTransition.setFromValue(0.0);
+                showFileRootTransition.setToValue(1.0);
+                hideEditorRootTransition.play();
+                showFileRootTransition.play();
+            }
+        });
     }
 
 
@@ -117,5 +212,32 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
 
     public void handle(ActionEvent event) {
+        FadeTransition hideFileRootTransition = new FadeTransition(Duration.millis(500), fileRoot);
+        hideFileRootTransition.setFromValue(1.0);
+        hideFileRootTransition.setToValue(0.0);
+
+        FadeTransition showEditorRootTransition = new FadeTransition(Duration.millis(500), editorRoot);
+        showEditorRootTransition.setFromValue(0.0);
+        showEditorRootTransition.setToValue(1.0);
+
+        showEditorRootTransition.play();
+        hideFileRootTransition.play();
+        root.getChildren().remove(fileRoot);
+
+        if(event.getSource()==this.exitBtn){
+            System.exit(0);
+        }
+        if(event.getSource()==this.addNodeBtn){
+            this.addNodeClicked= true;
+            System.out.print("true");
+        }
+    }
+
+    public boolean isAddNodeClicked() {
+        return addNodeClicked;
+    }
+
+    public void setAddNodeClicked(boolean addNodeClicked) {
+        this.addNodeClicked = addNodeClicked;
     }
 }
