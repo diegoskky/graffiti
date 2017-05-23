829d1efc2644b4cd0b7031616bb0c6be1b250abe
peel40 <oolivares15@alumnos.utalca.cl>
2017-05-23 19:12:36 -0400

    Auto-hide tooltip for Integrity button and code formatting.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -816,31 +816,5 @@ public class Controller implements Initializable {
         return alphabetChecked;
     }
 
-    private static void updateTooltipBehavior(double openDelay, double visibleDuration,
-                                              double closeDelay, boolean hideOnExit) {
-        try {
-            // Get the non public field "BEHAVIOR"
-            Field fieldBehavior = Tooltip.class.getDeclaredField("BEHAVIOR");
-            // Make the field accessible to be able to get and set its value
-            fieldBehavior.setAccessible(true);
-            // Get the value of the static field
-            Object objBehavior = fieldBehavior.get(null);
-            // Get the constructor of the private static inner class TooltipBehavior
-            Constructor<?> constructor = objBehavior.getClass().getDeclaredConstructor(
-                    javafx.util.Duration.class, javafx.util.Duration.class, javafx.util.Duration.class, boolean.class
-            );
-            // Make the constructor accessible to be able to invoke it
-            constructor.setAccessible(true);
-            // Create a new instance of the private static inner class TooltipBehavior
-            Object tooltipBehavior = constructor.newInstance(
-                    new javafx.util.Duration(openDelay), new javafx.util.Duration(visibleDuration),
-                    new javafx.util.Duration(closeDelay), hideOnExit
-            );
-            // Set the new instance of TooltipBehavior
-            fieldBehavior.set(null, tooltipBehavior);
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-    }
 
 }
